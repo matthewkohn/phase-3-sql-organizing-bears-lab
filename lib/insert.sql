3dce89f09522846c92f4351f1c0440bc707553cb
MattKohn <kohney@hotmail.com>
2022-04-15 10:57:11 -0600

    commit tree 702df4166509a5c8945e50df39e3b64e067bd78c

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 8, 'M', 'Onyx', 'Happy', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 4, 'M', 'White', 'Pissed', 0);
+INSERT INTO bears VALUES (3, 'Tabitha', 12, 'F', 'Striped', 'confused', 0);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 9, 'F', 'purple', 'bored', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 9, 'F', 'green', 'deceitful', 0);
+INSERT INTO bears VALUES (6, 'Grinch', 14, 'M', 'brown', 'grumpy', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 7, 'F', 'yellow', 'sweet', 1);
+INSERT INTO bears VALUES (8, null, 1, 'M', 'orange', 'hungry', 1);
